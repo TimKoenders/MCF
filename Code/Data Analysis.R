10a43019035a00505bfe7971bcac9fbf183ce5f5
Tim Koenders <129389073+TimKoenders@users.noreply.github.com>
2024-02-12 21:06:39 +0100

    Final update

--- Code/Data Analysis.R
+++ Code/Data Analysis.R
@@ -39,66 +39,76 @@ list.files()
 df <- read.csv("df_overall_imp_sen.csv")
 
 
-#### Data Analysis -------------------------------------------------------
+#### VAR and IRF's with baseline sentiment score -------------------------------------------------------
 
-# Cumulative results for h = 0
-ts_data <- ts(df[, c("Cum_Res_1d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
-# Fit VAR model
-var_model <- VAR(ts_data, p = 3, type = "const")
-# Print coefficients
-coefficients <- coef(var_model)
-residuals <- resid(var_model)
-# Print the coefficients 
-print(coefficients$Cum_Res_1d)
-
-# Cumulative results for h = 1
+## Cumulative results for h = 1
 ts_data <- ts(df[, c("Cum_Res_2d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
-# Fit VAR model
-var_model <- VAR(ts_data, p = 3, type = "const")
+# Standardize specific variables in ts_data
+variables_to_standardize <- c("Cum_Res_2d", "returns", "CommodityIndex", "VIX", "art")
+ts_data[, variables_to_standardize] <- scale(ts_data[, variables_to_standardize])
+# Fit VAR model for h = 1
+var_model <- VAR(ts_data, p = 1, type = "const")
 # Print coefficients
 coefficients <- coef(var_model)
 residuals <- resid(var_model)
 # Print the coefficients 
 print(coefficients$Cum_Res_2d)
+# Compute IRF's
+irf_result_1 <- irf(var_model, impulse = "sentiment_score", response = "Cum_Res_2d", n.ahead = 5)
+# Plot the impulse responses
+plot(irf_result_1, main = "IRF of cumulative returns (h=1) to sentiment score shock", ylab = "Response", xlab = "Time")
 
-# Cumulative results for h = 2
+## Cumulative results for h = 2
 ts_data <- ts(df[, c("Cum_Res_3d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
-# Fit VAR model
-var_model <- VAR(ts_data, p = 3, type = "const")
+# Standardize specific variables in ts_data
+variables_to_standardize <- c("Cum_Res_3d", "returns", "CommodityIndex", "VIX", "art")
+ts_data[, variables_to_standardize] <- scale(ts_data[, variables_to_standardize])
+# Fit VAR model for h = 2
+var_model <- VAR(ts_data, p = 1, type = "const")
 # Print coefficients
 coefficients <- coef(var_model)
 residuals <- resid(var_model)
 # Print the coefficients 
 print(coefficients$Cum_Res_3d)
+# Compute IRF's
+irf_result_2 <- irf(var_model, impulse = "sentiment_score", response = "Cum_Res_3d", n.ahead = 5)
+# Plot the impulse responses
+plot(irf_result_2, main = "IRF of cumulative returns (h=2) to sentiment score shock", ylab = "Response", xlab = "Time")
 
-# Cumulative results for h = 3
-ts_data <- ts(df[, c("Cum_Res_4d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
-# Fit VAR model
-var_model <- VAR(ts_data, p = 3, type = "const")
+
+#### VAR and IRF's with weighted sentiment score -------------------------------------------------------
+
+## Cumulative results for h = 1
+ts_data <- ts(df[, c("Cum_Res_2d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
+# Standardize specific variables in ts_data
+variables_to_standardize <- c("Cum_Res_2d", "returns", "CommodityIndex", "VIX", "art")
+ts_data[, variables_to_standardize] <- scale(ts_data[, variables_to_standardize])
+# Fit VAR model for h = 1
+var_model <- VAR(ts_data, p = 1, type = "const")
 # Print coefficients
 coefficients <- coef(var_model)
 residuals <- resid(var_model)
 # Print the coefficients 
-print(coefficients$Cum_Res_4d)
+print(coefficients$Cum_Res_2d)
+# Compute IRF's
+irf_result_1 <- irf(var_model, impulse = "sentiment_score_ws", response = "Cum_Res_2d", n.ahead = 5)
+# Plot the impulse responses
+plot(irf_result_1, main = "IRF of cumulative returns (h=1) to weighted sentiment score shock", ylab = "Response", xlab = "Time")
 
-# Cumulative results for h = 4
-ts_data <- ts(df[, c("Cum_Res_5d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
-# Fit VAR model
-var_model <- VAR(ts_data, p = 3, type = "const")
+## Cumulative results for h = 2
+ts_data <- ts(df[, c("Cum_Res_3d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
+# Standardize specific variables in ts_data
+variables_to_standardize <- c("Cum_Res_3d", "returns", "CommodityIndex", "VIX", "art")
+ts_data[, variables_to_standardize] <- scale(ts_data[, variables_to_standardize])
+# Fit VAR model for h = 2
+var_model <- VAR(ts_data, p = 1, type = "const")
 # Print coefficients
 coefficients <- coef(var_model)
 residuals <- resid(var_model)
 # Print the coefficients 
-print(coefficients$Cum_Res_5d)
-
-
-# Cumulative results for h = 5
-ts_data <- ts(df[, c("Cum_Res_6d", "returns", "CommodityIndex", "VIX", "sentiment_score", "sentiment_score_ws", "art")])
-# Fit VAR model
-var_model <- VAR(ts_data, p = 3, type = "const")
-# Print coefficients
-coefficients <- coef(var_model)
-residuals <- resid(var_model)
-# Print the coefficients 
-print(coefficients$Cum_Res_6d)
+print(coefficients$Cum_Res_3d)
+# Compute IRF's
+irf_result_2 <- irf(var_model, impulse = "sentiment_score_ws", response = "Cum_Res_3d", n.ahead = 5)
+# Plot the impulse responses
+plot(irf_result_2, main = "IRF of cumulative returns (h=2) to sentiment score shock", ylab = "Response", xlab = "Time")
 
